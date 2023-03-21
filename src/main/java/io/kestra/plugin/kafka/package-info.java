81cf88e53890f982575f639ddac40e85b292d5b2
Loïc Mathieu <loikeseke@gmail.com>
2023-03-21 14:32:58 +0100

    feat: plugin categories and icons

---
+++ src/main/java/io/kestra/plugin/kafka/package-info.java
@@ -0,0 +1,8 @@
+@PluginSubGroup(
+    description = "This sub-group of plugins contains tasks for using the Apache Kafka message broker.\n" +
+        "Apache Kafka is an open-source distributed event streaming platform used by thousands of companies for high-performance data pipelines, streaming analytics, data integration, and mission-critical applications.",
+    categories = PluginSubGroup.PluginCategory.MESSAGING
+)
+package io.kestra.plugin.kafka;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
