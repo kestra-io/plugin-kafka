db7acafb765e16071ca87630eecea5e10a066efe
YannC <ycoornaert@kestra.io>
2023-10-13 11:05:40 +0200

    doc(serdeType): uncomment serdeType so props using it have their doc displayed

--- src/main/java/io/kestra/plugin/kafka/serdes/SerdeType.java
+++ src/main/java/io/kestra/plugin/kafka/serdes/SerdeType.java
@@ -1,9 +1,10 @@
 package io.kestra.plugin.kafka.serdes;
 
-@io.swagger.v3.oas.annotations.media.Schema(
-    title = "Serializer / Deserializer to use for the value",
-    description = "Lists are not supported."
-)
+// FIXME: Find a way to interpret SerdeType as a class in markdown, to list it in 'Definitions'
+//@io.swagger.v3.oas.annotations.media.Schema(
+//    title = "Serializer / Deserializer to use for the value",
+//    description = "Lists are not supported."
+//)
 public enum SerdeType {
     STRING,
     INTEGER,
