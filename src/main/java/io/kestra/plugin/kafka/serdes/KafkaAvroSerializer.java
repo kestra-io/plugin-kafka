d1b91a95c01cd76384b2a805f363eecda7e41f09
Florian Hussonnois <florian.hussonnois@gmail.com>
2024-02-22 18:46:52 +0100

    build(deps): bump to confluent 7.5.1

--- src/main/java/io/kestra/plugin/kafka/serdes/KafkaAvroSerializer.java
+++ src/main/java/io/kestra/plugin/kafka/serdes/KafkaAvroSerializer.java
@@ -1,12 +1,41 @@
 package io.kestra.plugin.kafka.serdes;
 
-import org.apache.avro.Schema;
-import org.apache.avro.generic.GenericDatumWriter;
-import org.apache.avro.io.DatumWriter;
+import io.confluent.kafka.schemaregistry.avro.AvroSchemaUtils;
+import io.confluent.kafka.serializers.KafkaAvroSerializerConfig;
+import org.apache.avro.data.TimeConversions;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class KafkaAvroSerializer extends io.confluent.kafka.serializers.KafkaAvroSerializer {
+
+    static {
+        org.apache.avro.generic.GenericData genericData = AvroSchemaUtils.getGenericData();
+        genericData.addLogicalTypeConversion(new AdditionalConversions.ZonedDateTimeMillisConversion());
+        genericData.addLogicalTypeConversion(new AdditionalConversions.ZonedDateTimeMicrosConversion());
+        genericData.addLogicalTypeConversion(new AdditionalConversions.OffsetDateTimeMillisConversion());
+        genericData.addLogicalTypeConversion(new AdditionalConversions.OffsetDateTimeMicrosConversion());
+        // IMPORTANT - we need to (re)add TimeConversions to ensure that ZonedDateTime/OffsetDateTime
+        // do not override TimeConversions for deserialization
+        genericData.addLogicalTypeConversion(new TimeConversions.TimeMicrosConversion());
+        genericData.addLogicalTypeConversion(new TimeConversions.TimeMillisConversion());
+        genericData.addLogicalTypeConversion(new TimeConversions.TimestampMicrosConversion());
+        genericData.addLogicalTypeConversion(new TimeConversions.TimestampMillisConversion());
+        genericData.addLogicalTypeConversion(new TimeConversions.LocalTimestampMicrosConversion());
+        genericData.addLogicalTypeConversion(new TimeConversions.LocalTimestampMillisConversion());
+        genericData.addLogicalTypeConversion(new TimeConversions.LocalTimestampMicrosConversion());
+    }
+
+    /**
+     * {@inheritDoc}
+     **/
     @Override
-    protected DatumWriter<?> getDatumWriter(Object value, Schema schema) {
-        return new GenericDatumWriter<>(schema, GenericData.get());
+    public void configure(Map<String, ?> configs, boolean isKey) {
+        HashMap<String, Object> mutable = new HashMap<>(configs);
+        if (!mutable.containsKey(KafkaAvroSerializerConfig.AVRO_USE_LOGICAL_TYPE_CONVERTERS_CONFIG)) {
+            // by default, always use LogicalType
+            mutable.put(KafkaAvroSerializerConfig.AVRO_USE_LOGICAL_TYPE_CONVERTERS_CONFIG, true);
+        }
+        super.configure(mutable, isKey);
     }
 }
