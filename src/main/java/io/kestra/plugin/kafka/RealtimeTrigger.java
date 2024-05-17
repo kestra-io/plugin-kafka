978f04f5a67ad9b4103efb61b79ce5bc73124836
Florian Hussonnois <florian.hussonnois@gmail.com>
2024-05-17 18:00:54 +0200

    feat: enhance realtime trigger lifecycle
    part-of: kestra-io/kestra#3767

--- src/main/java/io/kestra/plugin/kafka/RealtimeTrigger.java
+++ src/main/java/io/kestra/plugin/kafka/RealtimeTrigger.java
@@ -4,13 +4,21 @@ import io.kestra.core.models.annotations.Example;
 import io.kestra.core.models.annotations.Plugin;
 import io.kestra.core.models.conditions.ConditionContext;
 import io.kestra.core.models.executions.Execution;
-import io.kestra.core.models.triggers.*;
+import io.kestra.core.models.triggers.AbstractTrigger;
+import io.kestra.core.models.triggers.RealtimeTriggerInterface;
+import io.kestra.core.models.triggers.TriggerContext;
+import io.kestra.core.models.triggers.TriggerOutput;
+import io.kestra.core.models.triggers.TriggerService;
 import io.kestra.core.runners.RunContext;
 import io.kestra.plugin.kafka.serdes.SerdeType;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.errors.WakeupException;
 import org.reactivestreams.Publisher;
 import reactor.core.publisher.Flux;
 
@@ -18,6 +26,10 @@ import java.time.Duration;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
 
 @SuperBuilder
 @ToString
@@ -73,6 +85,18 @@ public class RealtimeTrigger extends AbstractTrigger implements RealtimeTriggerI
 
     private Duration maxDuration;
 
+    @Builder.Default
+    @Getter(AccessLevel.NONE)
+    private final AtomicBoolean isActive = new AtomicBoolean(true);
+
+    @Builder.Default
+    @Getter(AccessLevel.NONE)
+    private final CountDownLatch waitForTermination = new CountDownLatch(1);
+
+    @Builder.Default
+    @Getter(AccessLevel.NONE)
+    private final AtomicReference<Consumer<Object, Object>> consumer = new AtomicReference<>();
+
     @Override
     public Publisher<Execution> evaluate(ConditionContext conditionContext, TriggerContext context) {
         RunContext runContext = conditionContext.getRunContext();
@@ -94,7 +118,69 @@ public class RealtimeTrigger extends AbstractTrigger implements RealtimeTriggerI
             .maxDuration(this.maxDuration)
             .build();
 
-        return Flux.from(task.stream(runContext))
+        return Flux.from(publisher(task, runContext))
             .map((record) -> TriggerService.generateRealtimeExecution(this, context, task.recordToMessage(record)));
+    }
+
+    public Publisher<ConsumerRecord<Object, Object>> publisher(final Consume task,
+                                                               final RunContext runContext) {
+        return Flux.create(fluxSink -> {
+            try (KafkaConsumer<Object, Object> consumer = task.consumer(runContext)) {
+                this.consumer.set(consumer);
+                task.topicSubscription(runContext).subscribe(consumer, task);
+                while (isActive.get()) {
+                    try {
+                        consumer.poll(task.getPollDuration()).forEach(fluxSink::next);
+                        consumer.commitSync();
+                    } catch (org.apache.kafka.common.errors.InterruptException e) {
+                        // ignore, this case is handle by next lines
+                    }
+                    // Check if the current thread has been interrupted before next poll.
+                    if (Thread.currentThread().isInterrupted()) {
+                        isActive.set(false); // proactively stop polling
+                    }
+                }
+            } catch (WakeupException e) {
+                // ignore and stop
+            } catch (Exception e) {
+                fluxSink.error(e);
+            } finally {
+                fluxSink.complete();
+                this.waitForTermination.countDown();
+            }
+        });
+    }
+
+    /**
+     * {@inheritDoc}
+     **/
+    @Override
+    public void kill() {
+        stop(true);
+    }
+
+    /**
+     * {@inheritDoc}
+     **/
+    @Override
+    public void stop() {
+        stop(false); // must be non-blocking
+    }
+
+    private void stop(boolean wait) {
+        if (!isActive.compareAndSet(true, false)) {
+            return;
+        }
+
+        Optional.ofNullable(consumer.get()).ifPresent(consumer -> {
+            consumer.wakeup();
+            if (wait) {
+                try {
+                    this.waitForTermination.await();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        });
     }
 }
