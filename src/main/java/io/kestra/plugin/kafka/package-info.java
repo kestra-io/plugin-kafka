283494d18166d6d257db39a4d210d12cd5688eb1
ben8t <pimpaudben@gmail.com>
2025-12-30 15:38:56 +0100

    chore: update plugin categories

--- src/main/java/io/kestra/plugin/kafka/package-info.java
+++ src/main/java/io/kestra/plugin/kafka/package-info.java
@@ -1,7 +1,11 @@
 @PluginSubGroup(
     description = "This sub-group of plugins contains tasks for using the Apache Kafka message broker.\n" +
         "Apache Kafka is an open-source distributed event streaming platform used by thousands of companies for high-performance data pipelines, streaming analytics, data integration, and mission-critical applications.",
-    categories = PluginSubGroup.PluginCategory.MESSAGING
+    categories = PluginSubGroup.PluginCategory.MESSAGING,
+    categories = {
+        PluginSubGroup.PluginCategory.DATA,
+        PluginSubGroup.PluginCategory.INFRASTRUCTURE
+    }
 )
 package io.kestra.plugin.kafka;
 
