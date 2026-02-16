12b34c0aff0873d13ec13ebd45f86b7411b57a2e
François Delbrayelle <fdelbrayelle@gmail.com>
2026-02-16 06:50:47 +0100

    chore: update plugin categories (#144)

--- src/main/java/io/kestra/plugin/kafka/package-info.java
+++ src/main/java/io/kestra/plugin/kafka/package-info.java
@@ -1,7 +1,9 @@
 @PluginSubGroup(
     description = "This sub-group of plugins contains tasks for using the Apache Kafka message broker.\n" +
-        "Apache Kafka is an open-source distributed event streaming platform used by thousands of companies for high-performance data pipelines, streaming analytics, data integration, and mission-critical applications.",
-    categories = PluginSubGroup.PluginCategory.MESSAGING
+        "Apache Kafka is an open-source distributed event streaming platform used by thousands of companies for high-performance data pipelines, streaming analytics, data integration, and mission-critical applications.",categories = {
+        PluginSubGroup.PluginCategory.DATA,
+        PluginSubGroup.PluginCategory.INFRASTRUCTURE
+    }
 )
 package io.kestra.plugin.kafka;
 
